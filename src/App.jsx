d26c34c65f14eeb12404062038988d5a512491a9
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-10-28 22:11:22 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 
 const App = () => {
   return <div>
-    <Header />
-    <Main />
-    <Footer />
+    <WelcomeMessage />
   </div>;
 }
 
