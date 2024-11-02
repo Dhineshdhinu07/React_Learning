9c16204b2e65d83b6b87bd7610ff01870988a84f
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-11-02 23:46:52 +0530

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import ProductList from "./components/ProductList";
-import UserList from "./components/UserList";
 
 const App = () => {
-    
-  return <div>
-    <UserList />
-    <ProductList />
-  </div>;
+    return <User
+    name = "Dhinesh"
+    age = {21}
+    details = "Learning Web Development"
+    />
+}
+
+const User = (props) => {
+  return<section>
+    <h1>{props.name}</h1>
+    <h2>Age: {props.age}</h2>
+    <h3>{props.details}</h3>
+  </section>
 }
 
 export default App;
